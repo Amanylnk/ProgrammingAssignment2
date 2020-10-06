7828aea02154e693eee9dbdaee8d0ab95fa4e83b
amany darkaoui <amany_darkaoui@outlook.fr>
2020-10-06 14:49:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" created with the above function. 
+## However, it first checks to see if the mean has already been calculated.
+##If so, it gets the mean from the cache and skips the computation. Otherwise, 
+##it calculates the mean of the data and sets the value of the mean in the cache via the setmean function
 
 makeCacheMatrix <- function(x = matrix()) {
-         m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+         inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL 
+  }
+  get <- function() (x)
+  setInverse <- function(inverse)(inv <<- inverse)
+  getInverse <- function()(inv)
+  list(set=set, get= get, setInverse= setInverse , getInverse= getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Cashsolve function:
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+        inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
